e11db5bef1b893621786e893637d1bd082e2bc36
Lluís Alemany Puig <lluisalemanypuig@users.noreply.github.com>
2018-04-25 17:23:23 +0200

    Added Constraint Programming part
    without any intermediate commit because I'm a moron and deleted the .git
    folder with all the commits from the private branch I had in my
    laptop...

---
+++ CP/src/heuristics/heuristic_private.cpp
@@ -0,0 +1,64 @@
+#include "heuristics.hpp"
+
+namespace heuristics {
+namespace heur_private {
+	
+	#define sanity_check(wb, error)					\
+		string msg;									\
+		bool sane = wb.is_sane(msg);				\
+		if (not sane) {								\
+			cout << "Solution not sane!" << endl;	\
+			cout << wb << endl << endl;				\
+			cout << msg << endl;					\
+			error = true;							\
+		}
+	
+	bool call_solver
+	(
+		Options& ops, const gifts& data,
+		size_t stop_at,
+		bool stop_time, double stop_when,
+		bool verbose,
+		wrapped_boxes& BEST, bool& error
+	)
+	{
+		if (stop_time) {
+			ops.stop = Stop::time( (unsigned long int)(1000.0*stop_when) );
+		}
+		
+		size_t n_sols;
+		box_wrapper_optim *boxes = nullptr;
+		box_wrapper_optim *sol;
+		
+		boxes = new box_wrapper_optim(data, BEST.get_roll_length());
+		BAB<box_wrapper_optim> INC_SOLV(boxes, ops);
+		delete boxes;
+		
+		n_sols = 0;
+		while (n_sols < stop_at and (sol = INC_SOLV.next()) and not error) {
+			wrapped_boxes wb;
+			sol->to_wrapped_boxes(data, wb);
+			
+			#ifdef BOX_DEBUG
+			sanity_check(wb, error)
+			#endif
+			
+			if (wb < BEST) {
+				BEST = wb;
+			}
+			
+			if (verbose) {
+				cout << n_sols + 1 << ")" << endl;
+				cout << wb << endl;
+				cout << endl;
+			}
+			
+			++n_sols;
+		}
+		
+		return n_sols > 0;
+	}
+	
+}
+}
+
---
+++ CP/src/utils/wrapped_boxes.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+/// C++ includes
+#include <iostream>
+#include <utility>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <limits>
+using namespace std;
+
+/// Custom includes
+#include "definitions.hpp"
+
+class wrapped_boxes {
+	private:
+		width W;
+		length L, roll_length;
+		size_t N;
+		
+		// roll[i][j] = k <-> box k occupies cell k
+		vector<vector<size_t> > roll;
+		
+		// tl_corner[k] = (i,j) <-> top-left corner of box k is (i,j)
+		vector<corner> tl_corner;
+		
+		// br_corner[k] = (i,j) <-> bottom-right corner of box k is (i,j)
+		vector<corner> br_corner;
+	
+	private:
+		length box_length(size_t box_idx) const;
+		width box_width(size_t box_idx) const;
+	
+		void display_box_cells(ostream& os) const;
+		void display_box_corners(ostream& os) const;
+	
+	public:
+		wrapped_boxes();
+		wrapped_boxes(size_t N, length L, width W);
+		~wrapped_boxes();
+		
+		void init(size_t N, length L, width W);
+		
+		/// OPERATORS
+		
+		wrapped_boxes& operator= (const wrapped_boxes& wb);
+		bool operator< (const wrapped_boxes& wb) const;
+		
+		inline friend
+		ostream& operator<< (ostream& os, const wrapped_boxes& wb) {
+			wb.display_box_cells(os);
+			os << endl;
+			wb.display_box_corners(os);
+			return os;
+		}
+		
+		// c: (i, j), where
+		//    0 <= i <= L - 1, with L the total length of the roll
+		//    0 <= j <= W - 1, with W the (fixed) width of the roll
+		void set_tl_box_corner(size_t k, const corner& c);
+		void set_br_box_corner(size_t k, const corner& c);
+		void set_box_cell(size_t k, const cell& c);
+		void set_roll_length(length L);
+		
+		/// GETTERS
+		
+		// makes a sanity check
+		bool is_sane(string& error_msg) const;
+		
+		// returns the roll's length used
+		length get_roll_length() const;
+		
+		/// I/O
+		
+		// stores in a file following the format
+		// spceified in the project's statement
+		void store(ofstream& fout) const;
+};
+
