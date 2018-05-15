69c3de3c7798682207e137198b1f09f8c0ff47b3
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-05-15 11:11:33 +0200

    Implemented solve() method
    This means that the attributes of type IloEnv, IloModel, IloCPlex
    commont to all solvers are now declared in the interface.
    Method to change the verbosity of the solver.

--- LP/src/solvers/box_solver.cpp
+++ LP/src/solvers/box_solver.cpp
@@ -3,6 +3,28 @@
 box_solver::box_solver() : is_solved(false) { }
 box_solver::~box_solver() {}
 
+/* FIND SOLUTION */
+
+void box_solver::solve() {
+	is_solved = cplex.solve();
+}
+
+/* MODIFIERS */
+
+void box_solver::set_verbose(bool v) {
+	if (v) {
+		cplex.setOut(cout);
+		cplex.setWarning(cerr);
+	}
+	else {
+		cplex.setOut(env.getNullStream());
+		cplex.setWarning(env.getNullStream());
+	}
+}
+
+/* GETTERS */
+
 bool box_solver::found_solution() const {
 	return is_solved;
 }
+
--- LP/src/solvers/box_solver.hpp
+++ LP/src/solvers/box_solver.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// CPLEX
+#include <ilcplex/ilocplex.h>
+ILOSTLBEGIN
+
 // Custom includes
 #include "utils/wrapped_boxes.hpp"
 #include "utils/definitions.hpp"
@@ -11,13 +15,26 @@ class box_solver {
 	protected:
 		bool is_solved;
 		
+		// CPLEX variables
+		IloEnv env;
+		IloModel model;
+		IloCplex cplex;
+		
 	public:
 		box_solver();
-		~box_solver();
+		virtual ~box_solver();
+		
+		/* FIND SOLUTION */
 		
 		virtual void init(const gifts& gs, length max_L = -1) = 0;
-		virtual void solve() = 0;
+		void solve();
 		virtual void solution(const gifts& boxes_to_wrap, wrapped_boxes& wb) const = 0;
+		
+		/* MODIFIERS */
+		
+		void set_verbose(bool v = true);
+		
+		/* GETTERS */
 		
 		bool found_solution() const;
 };
