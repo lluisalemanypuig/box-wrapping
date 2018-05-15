6200f028cf292b9d97fb2903faec09ba3f556d2b
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-05-15 11:14:03 +0200

    Implemented solver that uses rotations

--- LP/src/solvers/box_wrapper_rotate.cpp
+++ LP/src/solvers/box_wrapper_rotate.cpp
@@ -1,10 +1,9 @@
 #include "box_wrapper_rotate.hpp"
 
-/* PRIVATE */
+/* PROTECTED */
 
 void box_wrapper_rotate::span_cells_non_rot
 (
-	length L, width W,
 	int b, length i, width j,
 	length b_length, width b_width
 )
@@ -12,17 +11,9 @@ void box_wrapper_rotate::span_cells_non_rot
 	for (length ii = i; ii <= i + b_length - 1; ++ii) {
 		for (width jj = j; jj <= j + b_width - 1; ++jj) {
 			
-			/*
-			rel(*this,
-				(
-					(box_rotated[b] == 0)
-					&&
-					(box_corner[b*W*L + i*W + j] == 1)
-				)
-				>>
-				(box_cell[b*W*L + ii*W + jj] == 1)
+			model.add(
+				IloIfThen(env, ((R(b) == 0) && (X(b,i,j) == 1)), (C(b,ii,jj) == 1))
 			);
-			*/
 			
 		}
 	}
@@ -30,7 +21,6 @@ void box_wrapper_rotate::span_cells_non_rot
 
 void box_wrapper_rotate::span_cells_rot
 (
-	length L, width W,
 	int b, length i, width j,
 	length b_length, width b_width
 )
@@ -38,17 +28,9 @@ void box_wrapper_rotate::span_cells_rot
 	for (length ii = i; ii <= i + b_width - 1; ++ii) {
 		for (width jj = j; jj <= j + b_length - 1; ++jj) {
 			
-			/*
-			rel(*this,
-				(
-					(box_rotated[b] == 1)
-					&&
-					(box_corner[b*W*L + i*W + j] == 1)
-				)
-				>>
-				(box_cell[b*W*L + ii*W + jj] == 1)
+			model.add(
+				IloIfThen(env, ((R(b) == 1) && (X(b,i,j) == 1)), (C(b,ii,jj) == 1))
 			);
-			*/
 			
 		}
 	}
@@ -56,7 +38,6 @@ void box_wrapper_rotate::span_cells_rot
 
 void box_wrapper_rotate::span_cells_square
 (
-	length L, width W,
 	int b, length i, width j,
 	length b_length, width b_width
 )
@@ -64,59 +45,88 @@ void box_wrapper_rotate::span_cells_square
 	for (length ii = i; ii <= i + b_length - 1; ++ii) {
 		for (width jj = j; jj <= j + b_width - 1; ++jj) {
 			
-			/*
-			rel(*this,
-				(box_corner[b*W*L + i*W + j] == 1)
-				>>
-				(box_cell[b*W*L + ii*W + jj] == 1)
+			model.add(
+				IloIfThen(env, (X(b,i,j) == 1), (C(b,ii,jj) == 1))
 			);
-			*/
 			
 		}
 	}
 	
-	//rel(*this, box_rotated[b] == 0);
+	model.add(R(b) == 0);
 }
+
+void box_wrapper_rotate::add_objective() { }
 
 /* PUBLIC */
 
-box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
+box_wrapper_rotate::box_wrapper_rotate() : box_solver() {
+	
+}
+
+box_wrapper_rotate::box_wrapper_rotate(const box_wrapper_rotate& bw) : box_solver() {
+	box_rotated = bw.box_rotated;
+	box_cell = bw.box_cell;
+	box_corner = bw.box_corner;
+	L = bw.L;
+	W = bw.W;
+}
+
+box_wrapper_rotate::~box_wrapper_rotate() { }
+
+void box_wrapper_rotate::init(const gifts& data, length max_L) {
+	// Initialise CPLEX variabox_lengthes needed to create the model
+	env = IloEnv();
+	model = IloModel(env);
+	cplex = IloCplex(model);
+	
 	if (max_L < 0) {
-		upper_bound_L = inf_t<int>();
+		L = inf_t<int>();
 	}
 	else {
-		upper_bound_L = max_L;
+		L = max_L;
 	}
 	
 	const int N = data.total_boxes;
-	const width W = data.W;
-	const length L = min(upper_bound_L, data.get_max_length_ro());
+	W = data.W;
+	L = min(L, data.get_max_length_s());
 	
-	/*
 	// initialise arrays
-	box_rotated = BoolVarArray(*this, N, 0, 1);
-	box_cell = BoolVarArray(*this, N*W*L, 0, 1);
-	box_corner = BoolVarArray(*this, N*W*L, 0, 1);
+	box_cell = IloNumVarArray(env, N*W*L, 0, 1, ILOINT);
+	box_corner = IloNumVarArray(env, N*W*L, 0, 1, ILOINT);
+	box_rotated = IloNumVarArray(env, N, 0, 1, ILOINT);
 	
-	// make constraints
+	/// CONSTRAINTS
+	
+	/// (1). Exactly one corner per box
 	for (int b = 0; b < N; ++b) {
-		/// (1). Exactly one corner per box
-		rel(*this, sum(box_corner.slice(b*W*L, 1, W*L)) == 1);
-		
-		/// (2). At most one box per cell
+		IloExpr exa_X(env);
 		for (length i = 0; i < L; ++i) {
 			for (width j = 0; j < W; ++j) {
-				rel(*this, sum(box_cell.slice(b*W*L + i*W + j, W*L, N)) <= 1);
+				exa_X += X(b,i,j);
 			}
+		}
+		model.add(exa_X == 1);
+		exa_X.end();
+	}
+	
+	/// (2). At most one box per cell
+	for (length i = 0; i < L; ++i) {
+		for (width j = 0; j < W; ++j) {
+			IloExpr amo_C(env);
+			for (int b = 0; b < N; ++b) {
+				amo_C += C(b,i,j);
+			}
+			model.add(amo_C <= 1);
+			amo_C.end();
 		}
 	}
 	
-	/// (3). Placing the top-left corner of a box at (i,j)
+	/// (4). Placing the top-left corner of a box at (i,j)
 	/// makes the box occupy several cells of the roll.
 	for (int b = 0; b < N; ++b) {
 		const box& BOX = data.all_boxes[b];
-		length b_length = BOX.l;
-		width b_width = BOX.w;
+		length b_length = data.all_boxes[b].l;
+		width b_width = data.all_boxes[b].w;
 		
 		// for each cell to be occupied by box 'b' (without rotation)
 		for (length i = 0; i < L; ++i) {
@@ -128,7 +138,7 @@ box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
 					// consider only the cases where the whole box is within bounds
 					bool within = (i + b_length - 1 < L) and (j + b_width - 1 < W);
 					if (within) {
-						span_cells_square(L, W, b, i, j, b_length, b_width);
+						span_cells_square(b, i, j, b_length, b_width);
 					}
 				}
 				else {
@@ -137,24 +147,24 @@ box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
 					// consider only the cases where the whole box is within bounds
 					bool within1 = (i + b_length - 1 < L) and (j + b_width - 1 < W);
 					if (within1) {
-						span_cells_non_rot(L, W, b, i, j, b_length, b_width);
+						span_cells_non_rot(b, i, j, b_length, b_width);
 					}
 					
 					bool within2 = (i + b_width - 1 < L) and (j + b_length - 1 < W);
 					if (within2) {
-						span_cells_rot(L, W, b, i, j, b_length, b_width);
+						span_cells_rot(b, i, j, b_length, b_width);
 					}
 				}
 			}
 		}
 	}
 	
-	/// (4). Cannot place the top-left corner of a box at
+	/// (5). Cannot place the top-left corner of a box at
 	/// cell (i,j) if it will end up out of bounds
 	for (int b = 0; b < N; ++b) {
 		const box& BOX = data.all_boxes[b];
-		length b_length = BOX.l;
-		width b_width = BOX.w;
+		length b_length = data.all_boxes[b].l;
+		width b_width = data.all_boxes[b].w;
 		
 		// for each cell to be occupied by box 'b' (without rotation)
 		for (length i = 0; i < L; ++i) {
@@ -166,7 +176,7 @@ box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
 					// if the box is WITHIN limits then ignore
 					bool within = (i + b_length - 1 <= L - 1) and (j + b_width - 1 <= W - 1);
 					if (not within) {
-						rel(*this, box_corner[b*W*L + i*W + j] == 0);
+						model.add(X(b,i,j) == 0);
 					}
 				}
 				else {
@@ -177,19 +187,15 @@ box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
 					
 					bool within1 = (i + b_length - 1 <= L - 1) and (j + b_width - 1 <= W - 1);
 					if (not within1) {
-						rel(*this,
-							(box_rotated[b] == 0)
-							>>
-							(box_corner[b*W*L + i*W + j] == 0)
+						model.add(
+							IloIfThen(env, (R(b) == 0), (X(b,i,j) == 0))
 						);
 					}
 					
 					bool within2 = (i + b_width - 1 <= L - 1) and (j + b_length - 1 <= W - 1);
 					if (not within2) {
-						rel(*this,
-							(box_rotated[b] == 1)
-							>>
-							(box_corner[b*W*L + i*W + j] == 0)
+						model.add(
+							IloIfThen(env, (R(b) == 1), (X(b,i,j) == 0))
 						);
 					}
 				}
@@ -197,90 +203,50 @@ box_wrapper_rotate::box_wrapper_rotate(const gifts& data, length max_L) {
 		}
 	}
 	
-	// branching strategy:
-	// Do not add the variables in array box_cell: if a variable
-	// is not assigned consider it as a 0 "no box in that cell".
-	// This way we avoid the solver assigning 1's wherever it can
-	// (whether it is on the slice for box 1, or for box 2, ...)
-	branch(*this, box_rotated + box_corner, BOOL_VAR_NONE(), BOOL_VAL_MAX());
-	*/
-	
+	cplex = IloCplex(model);
 }
 
-box_wrapper_rotate::box_wrapper_rotate(box_wrapper_rotate& bw) {
-	
-	/*
-	box_rotated.update(*this, bw.box_rotated);
-	box_cell.update(*this, bw.box_cell);
-	box_corner.update(*this, bw.box_corner);
-	upper_bound_L = bw.upper_bound_L;
-	*/
-	
-}
-
-box_wrapper_rotate::~box_wrapper_rotate() {
-}
-
-void box_wrapper_rotate::to_wrapped_boxes(const gifts& data, wrapped_boxes& wb) const {
-	const size_t N = data.total_boxes;
-	const width W = data.W;
-	const length L = min(upper_bound_L, data.get_max_length_ro());
+void box_wrapper_rotate::solution(const gifts& data, wrapped_boxes& wb) const {
+	const int N = data.total_boxes;
 	
 	wb.init(N, L, W);
 	
-	for (size_t b = 0; b < N; ++b) {
+	for (int b = 0; b < N; ++b) {
 		bool found = false;
 		length pl = L + 1;
 		width pw = W + 1;
 		
-		for (length i = 0; i < L; ++i) {
-			for (width j = 0; j < W; ++j) {
-				
-				// for corner finding
-				if (not found) {
-					
-					/*
-					BoolVar BX(box_corner[b*W*L + i*W + j]);
-					if (BX.val() == 1) {
-						pl = i;
-						pw = j;
-						found = true;
-					}
-					*/
-					
-				}
-				
-				/*
-				// for roll copying
-				BoolVar BC(box_cell[b*W*L + i*W + j]);
-				if (not BC.none() and BC.val() == 1) {
-					wb.set_box_cell(b + 1, cell(i, j));
-				}
-				*/
+		size_t k = 0;
+		while (k < L*W and not found) {
+			if (cplex.getValue(box_corner[b*L*W + k]) == 1) {
+				pl = k/W;
+				pw = k%W;
+				found = true;
 			}
+			++k;
 		}
-		
-		wb.set_tl_box_corner(b, corner(pl,pw));
 		
 		length box_length = data.all_boxes[b].l;
 		width box_width = data.all_boxes[b].w;
 		
-		/*
-		BoolVar BR(box_rotated[b]);
-		assert(not BR.none());
-		
-		if (BR.val() == 0) {
-			wb.set_br_box_corner(b,
-				corner(pl + box_length - 1, pw + box_width - 1)
-			);
+		if (cplex.getValue(R(b)) == 1) {
+			box_length = data.all_boxes[b].w;
+			box_width = data.all_boxes[b].l;
 		}
-		else {
-			wb.set_br_box_corner(b,
-				corner(pl + box_width - 1, pw + box_length - 1)
-			);
-		}
-		*/
 		
+		for (length i = pl; i < pl + box_length; ++i) {
+			for (width j = pw; j < pw + box_width; ++j) {
+				wb.set_box_cell(b + 1, cell(i,j) );
+			}
+		}
+		
+		wb.set_tl_box_corner(b, corner(pl,pw));
+		wb.set_br_box_corner(b,
+			corner(
+				pl + box_length - 1,
+				pw + box_width - 1
+			)
+		);
 	}
 }
 
--- LP/src/solvers/box_wrapper_rotate.hpp
+++ LP/src/solvers/box_wrapper_rotate.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-// C includes
-#include <assert.h>
-
 // C++ includes
 #include <iostream>
 using namespace std;
@@ -16,14 +13,14 @@ ILOSTLBEGIN
 #include "utils/definitions.hpp"
 #include "utils/input_data.hpp"
 #include "utils/logger.hpp"
+#include "box_solver.hpp"
 
-class box_wrapper_rotate {
+class box_wrapper_rotate : public box_solver {
 	protected:
 		// constraints for spanning cells when box is rotable
 		// but it is not rotated
 		void span_cells_non_rot
 		(
-			length L, width W,
 			int b, length i, width j,
 			length b_length, width b_width
 		);
@@ -32,7 +29,6 @@ class box_wrapper_rotate {
 		// and it is rotated
 		void span_cells_rot
 		(
-			length L, width W,
 			int b, length i, width j,
 			length b_length, width b_width
 		);
@@ -40,39 +36,50 @@ class box_wrapper_rotate {
 		// constraints for spanning cells when box is square
 		void span_cells_square
 		(
-			length L, width W,
 			int b, length i, width j,
 			length b_length, width b_width
 		);
 		
 	protected:
-		/*
-		// box_rotated[k] = 0 <-> box k is placed in the way it is indicated
+		// box_rotated[b] = 0 <-> box b is placed in the way it is indicated
 		// 						  in the input (l,w)
-		// box_rotated[k] = 1 <-> box k is rotated (w,l)
-		BoolVarArray box_rotated;
+		// box_rotated[b] = 1 <-> box b is rotated (w,l)
+		IloNumVarArray box_rotated;
 		
-		// box_cell[k][i][j] = 1 <-> cell (i, j) is occupied by box k
-		BoolVarArray box_cell;
+		// box_cell[b][i][j] = 1 <-> cell (i, j) is occupied by box b
+		IloNumVarArray box_cell;
 		
-		// box_corner[k][i][j] = 1 <-> top-left corner of box k
+		// box_corner[b][i][j] = 1 <-> top-left corner of box b
 		// is at cell (i,j) with
-		//     1 <= j <= W - w_k (width)
-		//     1 <= i <= L - l_k (length)
+		//     1 <= j <= W - w_b (width)
+		//     1 <= i <= L - l_b (length)
 		// where
-		//     w_k is the width of box k
-		//     l_k is the length of box k
-		BoolVarArray box_corner;
-		*/
-	
+		//     w_b is the width of box b
+		//     l_b is the length of box b
+		IloNumVarArray box_corner;
+		
 		// upper bound on the roll's length
-		length upper_bound_L;
+		length L;
+		// roll's width. Fixed value
+		width W;
 	
+	protected:
+		IloNumVar X(size_t b, size_t i, size_t j) const { return box_corner[b*W*L + i*W + j]; }
+		IloNumVar C(size_t b, size_t i, size_t j) const { return box_cell[b*W*L + i*W + j]; }
+		IloNumVar R(size_t b) const { return box_rotated[b]; }
+		
+		IloNumVar X(size_t b, size_t i, size_t j) { return box_corner[b*W*L + i*W + j]; }
+		IloNumVar C(size_t b, size_t i, size_t j) { return box_cell[b*W*L + i*W + j]; }
+		IloNumVar R(size_t b) { return box_rotated[b]; }
+		
+		virtual void add_objective();
+		
 	public:
-		box_wrapper_rotate(const gifts& gs, length max_L = -1);
-		box_wrapper_rotate(box_wrapper_rotate& bw);
+		box_wrapper_rotate();
+		box_wrapper_rotate(const box_wrapper_rotate& bw);
 		virtual ~box_wrapper_rotate();
 		
-		void to_wrapped_boxes(const gifts& boxes_to_wrap, wrapped_boxes& wb) const;
+		void init(const gifts& gs, length max_L = -1);
+		void solution(const gifts& boxes_to_wrap, wrapped_boxes& wb) const;
 };
 
